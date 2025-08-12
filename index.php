21e34f0f431be96556e7aaab360b2642dc871859
dansiscool <danilofilgueira100@gmail.com>
2025-08-12 18:14:16 -0300

    Base sem CSS

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+?>
+<html>
+<head>
+<title>VOLT2U</title>
+
+
+</head>
+<body>
+<div class="criaconta">CRIAR CONTA</div>
+<h1>VOLT2U LOGIN</h1>
+<form action="" method="post">
+    EMAIL
+    <input name="email" placeholder="NOME@EXEMPLO.COM">
+    SENHA
+    <input name="senha" placeholder="SENHA">
+    <button>ENTRAR</button>
+</form>
+
+<button>CONTINUAR COM GOOGLE</button>
+<button>CONTINUAR COM FACEBOOK</button>
+
+</body>
+
+</html>
