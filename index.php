9f4da1f4a3923f6663cf95df5ace371d64849df5
dansiscool <danilofilgueira100@gmail.com>
2025-08-12 19:27:28 -0300

    CSS Parcialmente feito. Arrumar divisão do SIGNUP com as REDES de conexão.

--- index.php
+++ index.php
@@ -3,23 +3,99 @@
 <html>
 <head>
 <title>VOLT2U</title>
+<style>
+    .botaorede
+    {
+        cursor: pointer;
+        padding: 15px;
+        font-size: 18px;
+        font-weight: bold;
+        color: #fecb08;
+        background: transparent;
+        align-items: center;
+    }
+
+    input
+    {
+        background-color: black;
+        border: none;
+        color: #fecb08;
+        font-size: 18px;
+        padding: 8px;
+        outline: none;
+        font-size: 18px;
+    }
 
 
+    .botao
+    {
+        background-color: #32312e;
+        color: #fecb08;
+        font-size: 18px;
+        padding: 15px;
+    }
+
+    body
+    {
+        background-color: gray;
+    }
+    label
+    {
+        font-size: 16px;
+        font-weight: bold;
+        display: block;
+        margin-top: 10px;
+
+    }
+    .conjunto
+    {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .criaconta 
+    {
+        position: absolute;
+        top: 20px;
+        right: 30px;
+        cursor: pointer;
+        font-size: 21px;
+    }
+    .SIGNUP
+    {
+        display: flex;
+        flex-direction: column;
+        gap: 30px;
+    }
+</style>
 </head>
+
+
+
 <body>
 <div class="criaconta">CRIAR CONTA</div>
-<h1>VOLT2U LOGIN</h1>
-<form action="" method="post">
-    EMAIL
-    <input name="email" placeholder="NOME@EXEMPLO.COM">
-    SENHA
-    <input name="senha" placeholder="SENHA">
-    <button>ENTRAR</button>
-</form>
+<div class="conjunto">
+<div class="SIGNUP">
+    <div>
+        <label for="email">EMAIL</label>
+        <input type="email" id="" placeholder="NOME@EXEMPLO.COM">
+    </div>
+    <div>
+        <label for="senha">SENHA</label>
+        <input type="password" id="" placeholder="DIGITE SUA SENHA AQUI">
+    </div>
+    <button class="botao">ENTRAR</button>
+<div>
 
-<button>CONTINUAR COM GOOGLE</button>
-<button>CONTINUAR COM FACEBOOK</button>
+<div class="divisor">
+    <div class="TITULO">VOLT2U LOGIN</div>
+    <div class="linhadecima"></div>
+    <div class="logov2u">TESTE</div>
+    <div class="linhadebaixo"></div>
+</div>
 
-</body>
-
-</html>
+<div class="SIGNUPCOMREDES">
+    <button class="botaorede">CONTINUAR COM GOOGLE</button>
+    <button class="botaorede">CONTINUAR COM FACEBOOK</button>
+</div>
+</div>
